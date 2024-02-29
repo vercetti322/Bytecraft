a293770a66ed14099adecf2f96d3672c87e44dfd
vercetti322 <vercettitommy322@gmail.com>
2024-03-01 00:15:28 +0530

    started assignment_2

---
+++ Lexical Analysis/Lex/input.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    // declaration
+    int a, b, c;
+    /* assign 
+    values to the 
+    varaibles
+    
+    */
+    a=10; 
+    b=10;
+    
+    /* second 
+    multi-line
+    
+    \\test
+    */
+    
+    a=a+1; //increment a by 1;
+    
+    b=b+10; //increment b by 10;
+}
