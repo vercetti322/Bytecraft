286c86496d5ffdd0eb21a03e8df0000558855a07
Jatin Jindal <116031920+vercetti322@users.noreply.github.com>
2024-04-23 05:28:52 +0530

    run.sh done!

--- Pascal/run.sh
+++ Pascal/run.sh
@@ -4,4 +4,10 @@ yacc -d -t parser.y
 gcc -c lex.yy.c -o lex.yy.o
 gcc -c y.tab.c -o y.tab.o
 gcc lex.yy.o y.tab.o -o parser -ll
-./parser < sample_prog.txt
+output=$(./parser 2>&1)
+
+if echo "$output" | grep -q 'Error'; then
+    echo "$output" | awk '/Error/{flag=1} flag'
+else
+    echo 'CODE PARSED!!!'
+fi
